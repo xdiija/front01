f4f381a4d8b9cf8a49903fd19fd881aea6d370d2
xdiija <djalmaleandro@yahoo.com>
2022-02-02 10:46:46 -0300

    Criado login

---
+++ src/pages/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <p class="text-h2">Login</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
